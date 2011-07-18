53a6d9ce9e748cfeb8393df964fa8a66295c69be
pkolt <mail@pkolt.ru>
2011-07-18 11:50:33 +0600

    Bug fix context processors

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from hotspots.conf.settings import MAP_KEY
+from conf.settings import MAP_KEY
 
 def map_key(request):
     return {'MAP_KEY': MAP_KEY}
